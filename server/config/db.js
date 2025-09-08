90bcb4dc4c3b5ffaf97a41ff4e35f100bff2774a
Mahmoud-Ahmed200 <152525319+Mahmoud-Ahmed200@users.noreply.github.com>
2025-09-08 16:35:53 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,17 @@
-const { Pool } = require("pg");
+
+const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false, 
-  },
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = pool;
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.log('Database connection error:', err);
+  } else {
+    console.log('Database connected successfully');
+  }
+});
